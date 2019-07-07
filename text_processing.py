a529d382c5dd5d43780af54d3c42f0bdc5d65171
bulatkh <bulatkhaert@gmail.com>
2019-07-07 23:26:16 +0100

    minor import changes

--- text_processing.py
+++ text_processing.py
@@ -2,23 +2,8 @@ import os
 import pickle
 import re
 
+
 class Vocabulary(object):
-    """
-    The class is used to form a vocabulary (bag-of-words)
-    
-    Attributes:
-    -----------
-    number_of_words
-        current number of words in the class instance
-        
-    word_to_id
-        dictionary mapping words (tokens) to their ids
-        
-    id_to_word
-        dictionary mapping ids to the corresponding words
-    -----------
-    """
-    
     def __init__(self):
         self.number_of_words = 1
         self.word_to_id = dict()
@@ -29,12 +14,8 @@ class Vocabulary(object):
     def add_word(self, word):
         """
         Adds a word in the vocabulary
-        
-        Parameters:
-        -----------
-        word : str
-            The word to add
-        -----------
+
+        :param word: the word to add
         """
         if word not in self.word_to_id:
             self.word_to_id[word] = self.number_of_words
@@ -44,44 +25,30 @@ class Vocabulary(object):
     def get_id_by_word(self, word):
         """
         Returns id for an input word
-        
-        Parameters:
-        -----------
-        word : str
-            The word for which id is needed
-        -----------
+
+        :param word: The word corresponding to the requested id
+        :return: requested id
         """
         return self.word_to_id[word]
     
     def get_word_by_id(self, idx):
         """
         Returns a word for an input id
-        
-        Parameters:
-        -----------
-        id : int
-            The id for which word is needed
-        -----------
+
+        :param idx: The id of the requested word
+        :return: requested word
         """
         return self.id_to_word[idx]
     
     def save_vocabulary(self, filename_word_to_id='word_to_id.pickle', filename_id_to_word='id_to_word.pickle'):
         """
         Saves vocabulary dictionaries to pickle files
-        
-        Parameters:
-        -----------
-        filename_word_to_id : str
-            The filename for word_to_id dictionary
-        
-        filename_id_to_word : str
-            The filename for id_to_word dictionary
-        -----------
+
+        :param filename_word_to_id: The filename for word_to_id dictionary
+        :param filename_id_to_word: The filename for id_to_word dictionary
         """
-        try: 
+        if not os.path.exists('./vocabulary'):
             os.mkdir('./vocabulary')
-        except:
-            pass
         path_word_to_id = os.path.join('./vocabulary/', filename_word_to_id)
         path_id_to_word = os.path.join('./vocabulary/', filename_id_to_word)
         
@@ -91,18 +58,12 @@ class Vocabulary(object):
         with open(path_id_to_word, 'wb') as writer:
             pickle.dump(self.id_to_word, writer)
             
-    def load_vocabulary(self, path_word_to_id='./vocabulary/word_to_id.pickle', path_id_to_word='./vocabulary/id_to_word.pickle'):
+    def load_vocabulary(self, path_word_to_id, path_id_to_word):
         """
         Loads vocabulary dictionaries from pickle files
-        
-        Parameters:
-        -----------
-        path_word_to_id : str
-            The path to file with word_to_id dictionary
-        
-        filename_id_to_word : str
-            The path to file with id_to_word dictionary
-        -----------
+
+        :param path_word_to_id: The path to file with word_to_id dictionary
+        :param path_id_to_word: The path to file with id_to_word dictionary
         """
         with open(path_word_to_id, 'rb') as reader:
             self.word_to_id = pickle.load(reader)
@@ -115,36 +76,48 @@ class Vocabulary(object):
 
 def preprocess_captions(all_captions):
     """
-    Replaces all the signs by whitespaces
-    
-    Parameters:
-    -----------
-    all_captions: list
-        List of lists with all the captions
-    -----------
+    Replaces all the signs by whitespaces and transforms words to lowercase inplace
+
+    :param all_captions: List of lists with all the captions
     """
     for captions_list in all_captions:
         for i, caption in enumerate(captions_list):
             captions_list[i] = re.sub('\W+', ' ', caption.lower())
 
 
-def add_start_and_end_to_captions(all_captions, start_str = '<SOS>', end_str = '<EOS>'):
+def add_start_and_end_to_captions(all_captions, start_str='<SOS>', end_str='<EOS>'):
     """
-    Adds start and end of caption markers
-    
-    Parameters:
-    -----------
-    all_captions: list
-        List of lists with all the captions
-        
-    start_str: str
-        Start of caption marker
-        
-    end_str: str
-        End of caption marker
-    -----------
+    Adds start and end of caption markers with inplace
+
+    :param all_captions: List of lists with all the captions
+    :param start_str: Start of caption marker
+    :param end_str: End of caption marker
     """
     for captions in all_captions:
         for i in range(len(captions)):
-            captions[i] =  '{} {} {}'.format(start_str, captions[i], end_str)
+            captions[i] = '{} {} {}'.format(start_str, captions[i], end_str)
             captions[i] = captions[i].replace('  ', ' ').lower()
+
+
+def tokenise_captions(list_captions, vocabulary):
+    """
+    Transforms the list of captions for all images in a dataset
+
+    :param list_captions: list of captions for images in the dataset
+    :param vocabulary: Vocabulary instance for the dataset
+    :return: list of transformed captions
+    """
+    captions_tokens = []
+    for captions in list_captions:
+        tmp_captions_for_img = []
+        for caption in captions:
+            caption_words = caption.split()
+            tmp = []
+            for word in caption_words:
+                if word in vocabulary.word_to_id:
+                    tmp.append(vocabulary.get_id_by_word(word))
+                else:
+                    tmp.append(0)
+            tmp_captions_for_img.append(tmp)
+        captions_tokens.append(tmp_captions_for_img)
+    return captions_tokens
