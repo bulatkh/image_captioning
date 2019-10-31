51248a2310b838f5ea9ea838b72ee620b0709b25
bulatkh <bulatkhaert@gmail.com>
2019-10-31 22:29:38 +0300

    changed config structure for attention

--- configs/default.py
+++ configs/default.py
@@ -3,9 +3,13 @@ from yacs.config import CfgNode as CN
 _C = CN()
 _C.NAME = "default"
 _C.DATASET = "Flickr8k"
+_C.IMG_PATH = "D:/Flickr8k/images/"
+_C.ANNOTATIONS_PATH = "D:/Flickr8k/annotations/"
+_C.ATTENTION = True
 
 _C.ENCODER = CN()
-_C.ENCODER.ATTENTION = False
+_C.ENCODER.MODEL = 'VGG16'
+_C.ENCODER.BATCH_SIZE = 16
 
 _C.DECODER = CN()
 _C.DECODER.BATCH_SIZE = 32
@@ -17,7 +21,6 @@ _C.DECODER.BATCH_NORM = True
 _C.DECODER.DROPOUT = True
 # if false LSTM is used
 _C.DECODER.GRU = True
-_C.DECODER.ATTN = False
 _C.DECODER.ATTN_TYPE = "bahdanau"
 _C.DECODER.MAX_LEN = 30
 
